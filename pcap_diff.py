d401480b1fc514246e13795f0fd268f7bbb1d796
bastiaba <bastian.ballmann@inf.ethz.ch>
2016-03-30 16:58:44 +0200

    applied patches from czeck

--- pcap_diff.py
+++ pcap_diff.py
@@ -33,20 +33,24 @@ from scapy.all import rdpcap, wrpcap, Packet, NoPayload
 
 ###[ Parsing parameter ]###
 
-output_file = None
-input_files = []
-complete_diff = False
-ignore_source = False
-ignore_source_ip = False
-ignore_dest_ip = True
-ignore_source_mac = True
-ignore_macs = False
-ignore_seq_ack = False
-ignore_ip_id = False
-ignore_headers = []
-diff_only_left = False
-diff_only_right = False
-be_quite = False
+output_file       = None
+input_files       = []
+complete_diff     = False
+ignore_source     = False
+ignore_source_ip  = False
+ignore_dest_ip    = False
+ignore_source_mac = False
+ignore_macs       = False
+ignore_seq_ack    = False
+ignore_ip_id      = False
+ignore_timestamp  = False
+ignore_ttl        = False
+ignore_ck         = False
+ignore_headers    = []
+diff_only_left    = False
+diff_only_right   = False
+be_quiet          = False
+show_diffs        = False
 
 def usage():
     print sys.argv[0]
@@ -54,23 +58,27 @@ def usage():
     -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
     Diff two or more pcap files
     Programmed by Bastian Ballmann <bastian.ballmann@inf.ethz.ch>
-    Version 1.0.0
+    Version 1.1.0
     -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
-    -i <input_file> (use multiple times)
-    -o <output_file>
+    -i <input_file>  (use multiple times)
+    -o <output_file> (this is a pcap file)
     -c (complete diff, dont ignore ttl, checksums and timestamps) - False by default
-    -l diff only left side (first pcap file)
-    -r diff only right side (not first pcap file)
-    -f s (ignore src ip / mac) - False by default
-    -f sm (ignore src mac) - True by default
-    -f si (ignore src ip) - False by default
-    -f di (ignore dst ip) - False by default
-    -f m (ignore mac addresses) - False by default
-    -f sa (ignore tcp seq and ack num) - False by default
-    -f ii (ignore ip id) - False by default
+    -l     diff only left side (first pcap file)
+    -r     diff only right side (not first pcap file)
+    -f s   (ignore src ip / mac)
+    -f sm  (ignore src mac)
+    -f si  (ignore src ip)
+    -f di  (ignore dst ip)
+    -f m   (ignore mac addresses)
+    -f sa  (ignore tcp seq and ack num)
+    -f ii  (ignore ip id)
+    -f ts  (ignore timestamp)
+    -f ttl (ignore ttl)
+    -f ck  (ignore checksum)
     -f <scapy header name>
-    -q be quite
+    -q     be quiet
+    -d     show differences
 
     Example usage with ignore mac addresses
     pcap_diff.py -i client.dump -i server.dump -o diff.pcap -f m
@@ -79,7 +87,7 @@ def usage():
     sys.exit(1)
 
 try:
-    cmd_opts = "cf:i:lo:qr"
+    cmd_opts = "cf:i:lo:qrd"
     opts, args = getopt.getopt(sys.argv[1:], cmd_opts)
 except getopt.GetoptError:
     usage()
@@ -91,12 +99,14 @@ for opt in opts:
         output_file = opt[1]
     elif opt[0] == "-c":
         complete_diff = True
+    elif opt[0] == "-d":
+        show_diffs = True
     elif opt[0] == "-l":
         diff_only_left = True
     elif opt[0] == "-r":
         diff_only_right = True
     elif opt[0] == "-q":
-        be_quite = True
+        be_quiet = True
     elif opt[0] == "-f":
         if opt[1] == "s":
             ignore_source = True
@@ -106,12 +116,20 @@ for opt in opts:
             ignore_seq_ack = True
         elif opt[1] == "ii":
             ignore_ip_id = True
+            ignore_ck  = True
         elif opt[1] == "si":
             ignore_source_ip = True
         elif opt[1] == "di":
             ignore_dest_ip = True
         elif opt[1] == "m":
             ignore_macs = True
+        elif opt[1] == "ts":
+            ignore_timestamp = True
+        elif opt[1] == "ttl":
+            ignore_ttl = True
+            ignore_ck  = True
+        elif opt[1] == "ck":
+            ignore_ck = True
         else:
             ignore_headers.append(opt[1])
     else:
@@ -135,17 +153,30 @@ def flatten(d, parent_key=''):
     skip_fields = ['fieldtype', 'underlayer', 'initialized', 'fieldtype',
                    'default_fields', 'aliastypes', 'post_transforms',
                    'packetfields', 'overloaded_fields', 'sent_time']
-    
+
+    hasPayload = 'payload' in d
+
     for k, v in d.items():
         fullk = "%s_%s" % (parent_key, k)
+
+        #print "KEY=",parent_key, ">> key: ",k, " V=",v
+
+        # ignore the original if we have payload -- the payload will be expanded
+        if hasPayload and k == 'original':
+            continue
+
         # No complete diff? Ignore checksum, ttl and time
-        if not complete_diff and (k == "chksum" or k == "ttl" or k == "time"): 
+        if not complete_diff and (k == "chksum" or k == "ttl" or k == "time"):
+            continue
+
+        # ignore Timestamp
+        if ignore_timestamp and k == "time":
             continue
 
         # skip time value of deeper layers (they all get it from Packet)
         if parent_key and k == "time":
             continue
-        
+
         # Ignore source IP?
         if (ignore_source or ignore_source_ip) and k == "src":
             continue
@@ -160,6 +191,14 @@ def flatten(d, parent_key=''):
 
         # Ignore IP ID?
         if ignore_ip_id and k == "id":
+            continue
+
+        # Ignore Time To Live (TTL) field in IPv4 Header
+        if ignore_ttl and k == "ttl":
+            continue
+
+        # Ignore Checksum field in IPv4 Header
+        if ignore_ck and k == "chksum":
             continue
 
         # Ignore custom header field?
@@ -196,16 +235,18 @@ def serialize(packet):
     Serialize flattened packet
     """
     pdict = packet.__dict__
-    
+
     # remove mac addresses?
     if ignore_macs and pdict.get("fields"):
-       if pdict["fields"].get("src"): del pdict["fields"]["src"] 
+       if pdict["fields"].get("src"): del pdict["fields"]["src"]
        if pdict["fields"].get("dst"): del pdict["fields"]["dst"]
 
     if (ignore_source or ignore_source_mac) and pdict.get("fields"):
-        if pdict["fields"].get("src"): del pdict["fields"]["src"] 
+        if pdict["fields"].get("src"): del pdict["fields"]["src"]
 
     flat_packet = flatten(pdict)
+    ## print "\nPKT=",flat_packet
+
     serial = ""
 
     for key in sorted(flat_packet):
@@ -222,23 +263,46 @@ def read_dump(pcap_file):
     dump = {}
     count = 0
 
-    if not be_quite:
-        sys.stdout.write("Reading file " + pcap_file + "\n")
+    if not be_quiet:
+        sys.stdout.write("Reading file " + pcap_file + ":\n")
         sys.stdout.flush()
 
     for packet in rdpcap(pcap_file):
-        if not be_quite:
-            sys.stdout.write(":")
-            sys.stdout.flush()
-
         count += 1
         dump[serialize(packet)] = packet
 
-    if not be_quite:
-        sys.stdout.write("\nFound " + str(count) + " packets\n\n")
+    if not be_quiet:
+        sys.stdout.write("Found " + str(count) + " packets\n\n")
+        sys.stdout.flush()
 
     return dump
 
+def compare_summary ():
+    ret = ""
+    if complete_diff:
+        ret += "Complete, "
+    if ignore_source:
+        ret += "no src mac, no src ip, "
+    if ignore_source_mac:
+        ret += "no src mac, "
+    if ignore_source_ip:
+        ret += "no src ip, "
+    if ignore_dest_ip:
+        ret += "no dst ip, "
+    if ignore_seq_ack:
+        ret += "not seq ack, "
+    if ignore_ip_id:
+        ret += "no ip id, "
+    if ignore_ttl:
+        ret += "no ttl, "
+    if ignore_ck:
+        ret += "no checksum, "
+    if ignore_timestamp:
+        ret += "no timestamp, "
+    for h in ignore_headers:
+        ret += "no " + h + ", "
+
+    return ret
 
 ###[ MAIN PART ]###
 
@@ -253,24 +317,20 @@ diff_counter = 0
 diff_packets = []
 base_dump = dumps.pop(0)
 
-if not be_quite:
-    print "Diffing packets"
+if not be_quiet:
+    print "Diffing packets: " + compare_summary()
 
 for packet in base_dump.values():
     serial_packet = serialize(packet)
     found_packet = False
 
-    if not be_quite:
-        sys.stdout.write(":")
-        sys.stdout.flush()
-    
     for dump in dumps:
         if dump.get(serial_packet):
             del dump[serial_packet]
             found_packet = True
 
     if not diff_only_right and not found_packet:
-        if not be_quite:
+        if show_diffs:
             print " <<< " + packet.summary()
         diff_packets.append(packet)
 
@@ -279,15 +339,19 @@ if not diff_only_left:
         if len(dump.values()) > 0:
             diff_packets.extend(dump.values())
 
-            if not be_quite:
+            if show_diffs:
                 for packet in dump.values():
+                    packet.show()
                     print " >>> " + packet.summary()
 
-if not be_quite:
+if not be_quiet:
     print "\nFound " + str(len(diff_packets)) + " different packets\n"
 
 # Write pcap diff file?
 if output_file:
-    if not be_quite:
+    if not be_quiet:
         print "Writing " + output_file
     wrpcap(output_file, diff_packets)
+
+
+sys.exit (len(diff_packets))
